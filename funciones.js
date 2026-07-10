f42e21fda3f42dd3794900ad6f55f8eec36a9a9d
mapcom162 <domelinve@outlook.es>
2026-07-10 09:27:31 -0500

    Funcion calcularDisponible

--- funciones.js
+++ funciones.js
@@ -1,1 +1,11 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+
+function calcularDisponible (ingresos,egresos){
+    let total = ingresos - egresos;
+    if (total < 0){
+        return 0;
+    } else {
+        return total;
+    }
+}
+
--- simulador.js
+++ simulador.js
@@ -1,1 +1,14 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+
+function calcular(){
+    let cmpIngresos = document.getElementById("txtIngresos");
+    let ingresos = parseFloat(cmpIngresos.value);
+
+    let cmpEgresos = document.getElementById("txtEgresos");
+    let egresos = parseFloat(cmpEgresos.value);
+
+    let disponible = calcularDisponible(ingresos,egresos).toFixed(2);
+
+    let spnDisponible = document.getElementById("spnDisponible");
+    spnDisponible.textContent = disponible;
+}
